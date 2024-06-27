842133406b337a7d6f9bebcfd38c22cd803b2d6e
Aitor Tola <a.tola@kirol.es>
2024-06-27 08:32:47 +0200

    Cambios

--- app/src/main/java/com/aitortola/captaingame/MainActivity.kt
+++ app/src/main/java/com/aitortola/captaingame/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -23,10 +24,10 @@ import kotlin.random.Random
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             CaptainGAmeTheme {
-                Surface(modifier = Modifier.fillMaxSize(),
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background)
                 {
                     CaptainGame()
